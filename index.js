2cd4b424cb397a6db4cb88bad1b98601233613f1
marshall-bits <kz_@hotmail.es>
2024-06-18 19:40:53 +0200

    refactor first route

--- index.js
+++ index.js
@@ -1,54 +1,40 @@
 import express from "express";
 import logger from "morgan";
-import OpenAI from "openai";
 import { getHtmlInfo } from "./utils/formatInfo.js";
 import { categorizedPage } from "./utils/categorize.js";
 
 const app = express();
 app.use(logger("dev"));
 
-const test = async (req, res) => {
-  const { pageInfo } = req.query;
-
-  const stream = await categorizedPage(pageInfo);
-  if (stream.includes("Error")) {
-    res
-      .status(400)
-      .send({ message: "Error getting the categories for this page" });
-    return;
-  }
-  console.log(stream);
-  res.status(200).send(stream);
-};
-
 app.get("/", (req, res) => {
-  test(req, res);
   const url = req.query.url;
+  res.send(`The url is: ${url}`);
 });
 
-app.get("/scrap", (req, res) => {
+app.get("/scrap", async (req, res) => {
   const url = req.query.url;
 
-  const getHtml = async () => {
-    try {
-      const response = await fetch(url);
-      const html = await response.text();
-      req.query.html = html;
+  try {
+    const page = await fetch(url);
+    const html = await page.text();
+    const pageInfo = { ...getHtmlInfo(html), url };
+    const response = await categorizedPage(pageInfo);
 
-      req.query.pageInfo = { ...getHtmlInfo(html), url };
-
-      console.log(req.query.pageInfo);
-      // res.status(200).send({... getHtmlInfo(html), url});
-      test(req, res);
-    } catch {
-      res.status(400).send({
-        message:
-          "Unable to get info from this page, please make sure the url is correct",
-      });
+    if (response.includes("Error")) {
+      res
+        .status(400)
+        .send({ message: "Error getting the categories for this page" });
+      return;
     }
-  };
 
-  getHtml();
+    console.log(response);
+    res.status(200).send(response);
+  } catch {
+    res.status(400).send({
+      message:
+        "Unable to get info from this page, please make sure the url is correct",
+    });
+  }
 });
 
 app.listen(5005, () => {
