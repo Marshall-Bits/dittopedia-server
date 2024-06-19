d151adf8e68bae279f9359c7991d723c869bdbed
marshall-bits <kz_@hotmail.es>
2024-06-19 18:27:48 +0200

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import logger from "morgan";
+import cors from "cors";
 
 import { resourceRoutes } from "./routes/resource.routes.js";
 import { categorizeRoutes } from "./routes/categorize.routes.js";
@@ -7,9 +8,14 @@ import connectToDB from "./db/index.js";
 
 const app = express();
 connectToDB();
-
+app.set("trust proxy", 1);
 app.use(logger("dev"));
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/resource", resourceRoutes);
 app.use("/categorize", categorizeRoutes);
