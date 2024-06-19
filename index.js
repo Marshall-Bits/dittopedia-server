f9770e5f804a9c10ed22b5163b914e145c641dd6
marshall-bits <kz_@hotmail.es>
2024-06-19 11:27:31 +0200

    move categorize route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import logger from "morgan";
-import { getHtmlInfo } from "./utils/formatInfo.js";
-import { categorizedPage } from "./utils/categorize.js";
+
 import { resourceRoutes } from "./routes/resource.routes.js";
+import { categorizeRoutes } from "./routes/categorize.routes.js";
 import connectToDB from "./db/index.js";
 
 const app = express();
@@ -12,38 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 
 app.use("/resource", resourceRoutes);
-
-app.get("/", (req, res) => {
-  const url = req.query.url;
-  res.send(`The url is: ${url}`);
-});
-
-app.get("/categorize", async (req, res) => {
-  const url = req.query.url;
-
-  try {
-    const page = await fetch(url);
-    const html = await page.text();
-    const pageInfo = { ...getHtmlInfo(html), url };
-    const { favIcon } = pageInfo;
-    const response = await categorizedPage(pageInfo);
-
-    if (response.includes("Error")) {
-      res
-        .status(400)
-        .send({ message: "Error getting the categories for this page" });
-      return;
-    }
-
-    const parsedResponse = JSON.parse(response);
-    res.status(200).send({ ...parsedResponse, url, favIcon });
-  } catch {
-    res.status(400).send({
-      message:
-        "Unable to get info from this page, please make sure the url is correct",
-    });
-  }
-});
+app.use("/categorize", categorizeRoutes);
 
 app.listen(5005, () => {
   console.log("Server is running on port 5005");
---
+++ routes/categorize.routes.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { getHtmlInfo } from "../utils/formatInfo.js";
+import { categorizedPage } from "../utils/categorize.js";
+
+// ROUTES for /categorize
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const url = req.query.url;
+
+  try {
+    const page = await fetch(url);
+    const html = await page.text();
+    const pageInfo = { ...getHtmlInfo(html), url };
+    const { favIcon } = pageInfo;
+    const response = await categorizedPage(pageInfo);
+
+    if (response.includes("Error")) {
+      res
+        .status(400)
+        .send({ message: "Error getting the categories for this page" });
+      return;
+    }
+
+    const parsedResponse = JSON.parse(response);
+    res.status(200).send({ ...parsedResponse, url, favIcon });
+  } catch {
+    res.status(400).send({
+      message:
+        "Unable to get info from this page, please provide another URL or add the information manually.",
+    });
+  }
+});
+
+
+export { router as categorizeRoutes };
