6a3ea1dfb03c4e20180065641a489467f6d04680
marshall-bits <kz_@hotmail.es>
2024-06-21 10:11:24 +0200

    add extra check on favicon

--- utils/formatFavIcon.js
+++ utils/formatFavIcon.js
@@ -1,6 +1,8 @@
 const formatFavIcon = (iconUrl, pageUrl) => {
   const url = new URL(pageUrl);
   const baseUrl = `${url.protocol}//${url.hostname}`;
+  
+  if(!iconUrl) return null;
 
   if (iconUrl.startsWith("./") || iconUrl.startsWith("../")) {
     return `${baseUrl}/${iconUrl.replace("../", "")}`;
