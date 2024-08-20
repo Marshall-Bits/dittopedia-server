fb7e754ecba61ddea40aa5da55908a64fe9f9419
Marshall-Bits <kz_@hotmail.es>
2024-08-20 17:03:19 +0200

    get all categories sorted and by pages

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -7,11 +7,31 @@ const router = express.Router();
 
 // get the array of allCategories
 router.get("/", async (req, res) => {
+  let page = parseInt(req.query.page) || 1;
+
   const resources = await Resource.find({}, { categories: 1, _id: 0 });
 
   const allCategories = resources.map((resource) => resource.categories);
-  const uniqueCategories = [...new Set(allCategories.flat())];
-  res.json(uniqueCategories);
+  const uniqueCategories = [...new Set(allCategories.flat().sort())];
+
+  const limit = 8;
+  const pages = Math.ceil(uniqueCategories.length / limit);
+
+  if (page > pages) {
+    page = pages;
+  }
+
+  if (page < 1) {
+    page = 1;
+  }
+
+  const startIndex = (page - 1) * limit;
+
+  const endIndex = page * limit;
+
+  const results = uniqueCategories.slice(startIndex, endIndex);
+
+  res.json({ results, pages, page });
 });
 
 export { router as categoryRoutes };
